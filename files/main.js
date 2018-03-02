a4e41748750fa26695e2c412ea04be4ced6df197
miladmemarian <milad.memarian@gmail.com>
2018-03-02 15:45:39 -0800

    nextletter Phase

--- files/main.js
+++ files/main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
-function $text(text) {
+function $text(text, position) {
   var $text = document.createElement('div')
   $text.classList.add('text')
   $text.textContent = text
+  entry = text.split('')
+  $letter.textContent = entry[position]
   return $text
 }
 
@@ -11,10 +13,13 @@ function renderText(component, container) {
 }
 
 var $add = document.querySelector('.add')
+var $letter = document.querySelector('.letter')
+var position = 0
+var entry = []
 
 $add.addEventListener('click', function () {
   renderText(
-    $text(document.querySelector('.input').value),
+    $text(document.querySelector('.input').value, position),
     document.querySelector('.tutor')
   )
 })
