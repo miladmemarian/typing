4f31da0d320d310a1e2830946d0cadb06fc80250
miladmemarian <milad.memarian@gmail.com>
2018-03-05 12:01:38 -0800

    Improved Feedback function and added CSS animation

--- files/main.js
+++ files/main.js
@@ -2,10 +2,8 @@
 function $text(text, position) {
   var $text = document.createElement('div')
   $text.classList.add('text')
-  $text.textContent = text
-  entry = text.split('')
+  $text.textContent = text.join('')
   $letter.textContent = entry[position]
-  $feedback()
 
   return $text
 }
@@ -14,41 +12,31 @@ function renderText(component, container) {
   container.appendChild(component)
 }
 
-function $feedback() {
-  document.addEventListener('keydown', function (event) {
-    if (event.defaultPrevented) {
-      return
-    }
-    if (event.key === entry[position]) {
-      $letter.classList.add('correct')
-      setTimeout(function () {
-        $letter.classList.toggle('correct')
-      }, 200)
-    }
-    else {
-      $letter.classList.add('wrong')
-      setTimeout(function () {
-        $letter.classList.toggle('wrong')
-      }, 200)
-    }
-    $letter.textContent = entry[position]
-    event.preventDefault()
-  })
-  document.addEventListener('keyup', function (event) {
+document.addEventListener('keydown', function (event) {
+  if (event.target.classList.contains('input')) {
+    return
+  }
+  if (event.key === entry[position]) {
+    $letter.classList.add('correct')
+  }
+  else {
+    $letter.classList.add('wrong')
+  }
+  setTimeout(function () {
     position++
     $letter.textContent = entry[position]
-    event.preventDefault()
-  })
-}
+    $letter.classList.remove('wrong', 'correct')
+  }, 500)
+})
 
 var $add = document.querySelector('.add')
+
+$add.addEventListener('click', function () {
+  $add.blur()
+  entry = document.querySelector('.input').value.split('')
+  renderText($text(entry, position), document.querySelector('.tutor'))
+})
+
 var $letter = document.querySelector('.letter')
 var position = 0
 var entry = []
-
-$add.addEventListener('click', function () {
-  renderText(
-    $text(document.querySelector('.input').value, position),
-    document.querySelector('.tutor')
-  )
-})
